6de692e9ca05cf1f4e7bfe16dcfe0bd7a4f44ba0
EmadAnwer <emadanwer.official@gmail.com>
2024-01-18 11:56:26 +0200

    [fix] change user id pram name !! task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,12 +3,11 @@
 
 
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 BEGIN
     UPDATE users 
-    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
-    WHERE id = user_id;
-
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = p_user_id)
+    WHERE id = p_user_id;
 END //
 DELIMITER ;
 
