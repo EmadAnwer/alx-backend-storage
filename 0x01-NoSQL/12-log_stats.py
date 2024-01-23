090cb60d2670ae60af0f7fdd5fb62a246afe02cd
EmadAnwer <emadanwer.official@gmail.com>
2024-01-23 14:15:35 +0200

    [add] task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" 12-log_stats """
+from pymongo import MongoClient
+from pymongo.collection import Collection
+
+if __name__ == "__main__":
+    client: MongoClient = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection: Collection = client.logs.nginx
+
+    # nginx collection count
+    print(f"{nginx_collection.count_documents({})} logs")
+    method: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for m in method:
+        c: int = nginx_collection.count_documents({"method": m})
+        print(f"\tmethod {m}: {c}")
+
+    count: int = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{count} status check")
