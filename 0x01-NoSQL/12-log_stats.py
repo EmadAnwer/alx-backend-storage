9b0dbc8576478087d0f7a791cfa931f70d759be3
EmadAnwer <emadanwer.official@gmail.com>
2024-01-23 14:22:08 +0200

    [fix] task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,17 +5,15 @@ from pymongo.collection import Collection
 
 if __name__ == "__main__":
     client: MongoClient = MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection: Collection = client.logs.nginx
+    nginx_c: Collection = client.logs.nginx
 
     # nginx collection count
-    print(f"{nginx_collection.count_documents({})} logs")
+    print(f"{nginx_c.count_documents({})} logs")
     method: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for m in method:
-        c: int = nginx_collection.count_documents({"method": m})
+        c: int = nginx_c.count_documents({"method": m})
         print(f"\tmethod {m}: {c}")
 
-    count: int = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-        )
+    count: int = nginx_c.count_documents({"method": "GET", "path": "/status"})
     print(f"{count} status check")
