99e136701146bb9bf3dd2b30c3ef5ea10b63d4f6
EmadAnwer <emadanwer.official@gmail.com>
2024-01-23 13:23:39 +0200

    [fix] task 10 use many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,8 +3,10 @@
     10-update_topics.py
 """
 
+from pymongo.collection import Collection
 
-def update_topics(mongo_collection, name, topics):
+
+def update_topics(mongo_collection: Collection, name, topics):
     """changes all topics of a school document based on the name"""
 
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
